66ee8346b7186a236d29a8da393e543f2fac292b
Tapan Sharma <tapansharma69@gmail.com>
2024-05-06 22:14:54 +0545

    title and meta tags

--- resources/js/Pages/Posts/Index.jsx
+++ resources/js/Pages/Posts/Index.jsx
@@ -7,7 +7,9 @@ export default function Dashboard({ auth }) {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
-            <Head title="Posts" />
+            <Head title="Posts">
+                <meta name="description" content="Posts Index" />
+            </Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
