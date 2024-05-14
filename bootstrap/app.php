1e1c2225f7cdaa0526e8a4c0568a0783d3a291ec
Tapan Sharma <tapansharma69@gmail.com>
2024-05-14 12:52:44 +0545

    handling multiple error types with a single page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,8 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response) {
-            if ($response->getStatusCode() === 403) {
-                return Inertia::render("Error");
+            if (in_array($response->getStatusCode(), [403, 404])) {
+                return Inertia::render("Error", [
+                    'status' => $response->getStatusCode(),
+                ]);
             }
 
             return $response;
--- resources/js/Pages/Error.jsx
+++ resources/js/Pages/Error.jsx
@@ -1,12 +1,30 @@
 import GuestLayout from "@/Layouts/GuestLayout";
 import { Head } from "@inertiajs/react";
+import { useMemo } from "react";
 
-export default function Error() {
+export default function Error({ status }) {
+    const title = useMemo(() => {
+        return (
+            {
+                404: "Page Not Found",
+                403: "Forbidden",
+            }[status] || "An Error Occurred"
+        );
+    }, [status]);
+
+    const description = useMemo(() => {
+        return (
+            {
+                404: "The page you are looking for does not exist.",
+                403: "You are not allowed to perform this action.",
+            }[status] || "An Error Occurred"
+        );
+    }, [status]);
     return (
         <GuestLayout>
-            <Head title="An error occured" />
+            <Head title={title} />
             <div class="mb-4 font-medium text-sm text-red-600">
-                An Error Occurred
+                {description}
             </div>
         </GuestLayout>
     );
